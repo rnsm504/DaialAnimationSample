030c29f4dfcb87379c4c31beeef86b7523fa48c8
久世雅則 <masazak@gmail.com>
2016-10-30 11:38:07 +0900

    Initial Commit

---
+++ DaialAnimationSampleTests/DaialAnimationSampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  DaialAnimationSampleTests.swift
+//  DaialAnimationSampleTests
+//
+//  Created by Masanori Kuze on 2016/10/30.
+//  Copyright © 2016年 Masanori Kuze. All rights reserved.
+//
+
+import XCTest
+@testable import DaialAnimationSample
+
+class DaialAnimationSampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
